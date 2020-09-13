b775032860f71854e901d4f6ee6660f22d619070
psw0962 <65061325+psw0962@users.noreply.github.com>
2020-09-13 15:19:34 +0900

    Updates

---
+++ precache-manifest.da848395970a44a991b20bf05cb45d7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa09a1fb8ef2fed4f15acfdecc53131b",
+    "url": "/pokemon_react/index.html"
+  },
+  {
+    "revision": "6d3b38422465a0e72cb6",
+    "url": "/pokemon_react/static/js/2.cfff123a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon_react/static/js/2.cfff123a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d525e9ff9f80d7aff77",
+    "url": "/pokemon_react/static/js/main.59d0e5fa.chunk.js"
+  },
+  {
+    "revision": "23ce99f4823405f5a4a7",
+    "url": "/pokemon_react/static/js/runtime-main.31d54997.js"
+  }
+]);
---
+++ static/js/main.59d0e5fa.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppokemon_react=this.webpackJsonppokemon_react||[]).push([[0],{3:function(e,n,t){e.exports=t(8)},8:function(e,n,t){"use strict";t.r(n);var o=t(0),r=t.n(o),a=t(2);var c=function(){return r.a.createElement("div",null,"Hello")};t.n(a).a.render(r.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.59d0e5fa.chunk.js.map
